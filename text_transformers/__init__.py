738a9ba8192dc2366500372989b650814aeba740
Milan Kundra <kundra.milan@uk.sika.com>
2024-09-25 14:10:09 +0100

    Delete text_transformers directory

--- text_transformers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .embedding_transformers import EmbeddingCache, TextEmbedder
-from .keyword_transformers import TextTDFIF, TextBM25
-
-__all__ = [
-    EmbeddingCache, 
-    TextEmbedder,
-    TextTDFIF, 
-    TextBM25,
-]
